062f5b43941744e2b147e644f8727cc3cd0ed08e
Jason Kim <kimjeonghyon@gmail.com>
2020-12-09 13:56:09 +0900

    movie vo

---
+++ tableview/MovieVO.swift
@@ -0,0 +1,23 @@
+//
+//  MovieVO.swift
+//  tableview
+//
+//  Created by Jeonghyon Kim on 2020/12/06.
+//
+
+import Foundation
+
+
+class MovieVO {
+    init () {
+        
+    }
+    
+    var thumnail : String?
+    var title : String?
+    var description : String?
+    var detail : String?
+    var opendate : String?
+    var rating : Float?
+    
+}
